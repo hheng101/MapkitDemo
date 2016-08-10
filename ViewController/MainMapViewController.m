b1084a9366c011f60f9b4ccb5cbb4a7851cc63cd
hheng101 <409573994@qq.com>
2016-08-10 17:26:46 +0800

    增加中心点

--- ViewController/MainMapViewController.m
+++ ViewController/MainMapViewController.m
@@ -46,12 +46,16 @@
     [self requestLocation];
     
     self.mapView = [[MKMapView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
     self.mapView.delegate = self;
     
     self.mapView.userTrackingMode = MKUserTrackingModeFollow;
     self.mapView.mapType = MKMapTypeStandard;
     
     [self.view addSubview:self.mapView];
+    
+    [self addCenterImage];
+    
 }
 
 
@@ -72,24 +76,41 @@
 }
 
 
+///假装加个中心点的大头针
+- (void)addCenterImage {
+    
+    UIImage *image = [UIImage imageNamed:@"zhunxing"];
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+    CGRect centerRect = CGRectMake(self.view.center.x, self.view.center.y, 30, 30);
+    imageView.contentMode = UIViewContentModeScaleToFill;
+    imageView.frame = centerRect;
+    [self.view addSubview:imageView];
+}
+
+
 #pragma mark - 实例方法
 
 ///添加大头针
 - (void)addAnnotaions {
     
-    CLLocationCoordinate2D location1 = CLLocationCoordinate2DMake(39, 117);
+    CLLocationCoordinate2D location1 = CLLocationCoordinate2DMake(29, 106);
     Annotation *annotation1 = [[Annotation alloc] init];
     annotation1.title = @"老司机培训基地";
     annotation1.subtitle = @"老司机培训基地";
     annotation1.coordinate = location1;
     [self.mapView addAnnotation:annotation1];
     
-    CLLocationCoordinate2D location2 = CLLocationCoordinate2DMake(39.1, 117.1);
+    CLLocationCoordinate2D location2 = CLLocationCoordinate2DMake(29.1, 106.1);
     Annotation *annotation2 = [[Annotation alloc] init];
     annotation2.title = @"老司机培训基地";
     annotation2.subtitle = @"老司机培训基地";
     annotation2.coordinate = location2;
     [self.mapView addAnnotation:annotation2];
+    
+    CLLocationCoordinate2D centerCoordinate = self.mapView.region.center;
+    Annotation *centerAnnotation = [[Annotation alloc] init];
+    centerAnnotation.coordinate = centerCoordinate;
+    [self.mapView addAnnotation:centerAnnotation];
 }
 
 
@@ -98,6 +119,15 @@
 - (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
     
     NSLog(@"%@", userLocation);
+    
+}
+
+
+- (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated {
+    
+    CLLocationCoordinate2D centerCoordinate = mapView.region.center;
+    //可以用这个方法实现中心点的大头针效果
+    NSLog(@" regionDidChangeAnimated %f,%f",centerCoordinate.latitude, centerCoordinate.longitude);
 }
 
 
